9457410159a502b992233e1a93260236fe9caa5e
kuisatz <kuisatzhaderah@gmail.com>
2016-10-05 11:52:40 +0300

    BLL lisans değişimi

--- vendor/bll/bll/SysVillage.php
+++ vendor/bll/bll/SysVillage.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * OSTİM TEKNOLOJİ Framework 
+ * OSB İMALAT Framework 
  *
  * @link      https://github.com/corner82/slim_test for the canonical source repository
- * @copyright Copyright (c) 2015 OSTİM TEKNOLOJİ (http://www.ostim.com.tr)
- * @license  Ğİ 
+ * @copyright Copyright (c) 2015 OSB İMALAT (http://www.uretimosb.com)
+ * @license   
  */
 
 namespace BLL\BLL;
